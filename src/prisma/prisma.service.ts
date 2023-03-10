f9e489b47a8d81212aff29c4e1a400e758613486
Sulayman <sirikone@hotmail.com>
2023-03-10 15:41:17 +0100

    configservice ajouté

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { BookMark, PrismaClient, User } from '@prisma/client';
 
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(private _config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: "postgresql://postgres:123@localhost:5434/nest?schema=public"
+                    url: _config.get('DATABASE_URL')
                 }
             }
 
