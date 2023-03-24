8d51ec779c291fcb6c50d9d8d29b76546c4cbe9e
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2023-03-24 09:52:08 +0100

    controllers logic and service logic

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,15 +1,49 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 
+import { BookmarkService } from './bookmark.service';
 import { JwtGuard } from './../auth/guard/jwt.guard';
+import { GetUser } from '../auth/decorateur';
+import { CreateBookmarkDTO, EditBookmarkDTO } from './dto';
 
 @UseGuards(JwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
 
-    constructor() { }
+    constructor(private _bookmarkService: BookmarkService) { }
 
-    // @Get()
-    // getBo
+    // * Créer un marque page
+    @Post()
+    createBookmark(@GetUser('id') userID: number, @Body() dto: CreateBookmarkDTO) {
+return this._bookmarkService.createBookmark(userID,dto)
+    }
+    // * Chercher les marques pages
+    @Get()
+    getBookmarks(@GetUser('id') userID: number) {
+return this._bookmarkService.getBookmarks(userID)
+    }
+    @Get(':id')
+    getBookmarkByID(
+        @GetUser('id') userID: number,
+        @Param('id', ParseIntPipe) bookmarkID: number
+    ) {
+return this._bookmarkService.getBookmarkByID(userID,bookmarkID)
+    }
 
+    // * Mettre à jour le marque page
+    @Patch(':id')
+    editBookmark(
+        @GetUser('id') userID: number,bookmarkID:number,
+        @Body() dto: EditBookmarkDTO
+    ) {
+return this._bookmarkService.editBookmark(userID,bookmarkID,dto)
+    }
+
+    // * Supprimer le marque page
+    @Delete(':id')
+    deleteBookmark(@GetUser('id') userID: number,
+        @Param('id', ParseIntPipe) bookmarkID: number
+    ) {
+return this._bookmarkService.deleteBookmark(userID,bookmarkID)
+    }
 
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,52 @@
+import { CreateBookmarkDTO, EditBookmarkDTO } from './dto';
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+
+
+
+// * Créer un marque page
+
+createBookmark(
+    userID:number,
+    dto:CreateBookmarkDTO
+    ) {
+
+}
+// * Chercher les marques pages
+
+getBookmarks(userID:number) {
+
+}
+getBookmarkByID(
+    userID:number,
+    bookmarkID:number
+    ) {
+
+}
+
+// * Mettre à jour le marque page
+
+editBookmark(
+    userID:number,
+    bookmarkID: number,
+    dto : EditBookmarkDTO
+    ) {
+
+}
+
+// * Supprimer le marque page
+
+deleteBookmark(
+    userID:number,
+    bookmarkID: number
+    ) {
+
+}
+
+
+
+
+}
